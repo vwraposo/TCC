93061d70aa0d9a844a731d6a6a25ff273e61e277
Victor Wichmann Raposo <vwraposo@gmail.com>
2018-04-27 18:58:27 -0300

    Comandos Mr Bayes para DNA

--- src/nexus.py
+++ src/nexus.py
@@ -5,21 +5,35 @@
 ##################################################################################
 
 from string import Template
-template = Template("""#NEXUS
+
+data_template = Template("""#NEXUS
 
 Begin data;
         Dimensions ntax=$ntax nchar=$nchar;
-        Format datatype=$datatype interleave=yes gap=- missing=?;
+        Format datacharsete=$datatype interleave=yes gap=- missing=?;
         Matrix
 $matrix
     ;
 End;
 """)
 
+mb_template = Template("""begin mrbayes;
+    $partition
+
+    $code
+
+end;
+""")
+
+
 
 ## Problemas
 ## - So permite receber agrupado por tipo, nao pode recever DNA, Standard e DNA (sera que e problema? Daria para usar uma lista e checar a ultima entrada
 ## - Pode dar override (Talvez nao seja problema)
+
+# TODO:
+## - Fazer um format com classe
+## - partition dicionario datatype para partititons
 
 class NexusWriter:
 
@@ -27,33 +41,42 @@ class NexusWriter:
         self.taxa = set()
         self.data = dict()
         self._nchar = 1
-        self._format = dict()
+        self._format = dict() # { <datatype> : [<begin>, <end>] }
+        self._partition = dict() # { <charset> : [<begin>, <end>, <datatype>] }
 
     # Function to add a new data entry, recieves a taxon, the type of data and the sequence. 
-    def add (self, taxa, typ, datatype, seq):
+    def add (self, taxa, charset, datatype, seq):
         # Updating datastructures
         self.taxa.add(taxa)
-        if typ not in self.data:
+        if charset not in self.data:
+            self._partition[charset] = [str(self._nchar), "", datatype]
+            self.data[charset] = dict()
             if datatype not in self._format:
                 self._format[datatype] = [str(self._nchar), ""]
+
             self._nchar += len(seq)
-            self._format[datatype][1] = str(self._nchar)
-            self.data[typ] = dict()
+            self._format[datatype][1] = str(self._nchar - 1)
+            self._partition[charset][1] = str(self._nchar - 1)
 
-        self.data[typ][taxa] = seq
-
+        self.data[charset][taxa] = seq
 
 
     # Function that writes the nexus file
     def writeFile (self, outfile):
-        s = template.substitute(
+        data = data_template.substitute(
             ntax = len(self.taxa), 
             nchar = self._nchar, 
             datatype = self._makeFormat(),
             matrix = self._makeMatrix()
                 )
+
+        code = mb_template.substitute(
+            partition = self._makePartition(), 
+            code = self._makeCode()
+            )
         f = open(outfile, 'w')
-        f.write(s)
+        f.write(data)
+        f.write(code)
         f.close()
 
     # Function to convert the data matrix into string
@@ -74,3 +97,32 @@ class NexusWriter:
             out.append(it[0] + ":" + '-'.join(it[1]))
         return "mixed(" + ','.join(out) + ')'
         
+    # Function to convert the partition dictionary into string
+    def _makePartition (self):
+        s = []
+        for cset in self._partition.items():
+            s.append("\tcharset {0} = {1};".format(cset[0], '-'.join(cset[1])))
+        s.append("\tpartition part = {0}: {1};".format(str(len(self._partition)), " ".join(list(self._partition.keys()))))
+        s.append("\tset partition = part;")
+        return '\n'.join(s)
+    
+    # Function to convert the partition dictionary into string
+    def _makeCode (self):
+        c = []
+        part = dict()
+        for d in self._format:
+            part[d] = []
+        i = 1
+        for cset in self._partition.items():
+            # Garante ordem certa??
+            part[cset[1][2]].append(i)
+            i+= 1
+        if 'DNA' in part:
+            c.append("\tlset applyto=({0}) nst=mixed rates=invgamma;".format(",".join(str(x) for x in part['DNA'])))
+        c.append("\tunlink revmat=(all) pinvar=(all) shape=(all) statefreq=(all);")
+        c.append("\tprset ratepr=variable;")
+        return '\n'.join(c)
+
+
+
+                 
