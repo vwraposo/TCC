e744908d5df1dafb3239bf4422f5fc868cedd69f
Victor Wichmann Raposo <vwraposo@gmail.com>
2018-05-02 14:06:51 -0300

    Refatoramento do script de gerar arquivo NEXUS

--- src/MBinput.py
+++ src/MBinput.py
@@ -15,9 +15,15 @@ records = al.getAlignedSeq('16S')
 n = NexusWriter()
 for rec in records:
     n.add(rec.description.split()[2], '16S', 'DNA', str(rec.seq))
-    n.add(rec.description.split()[2], 'morph', 'Standard', '01002010010300405032110123')
+    n.add(rec.description.split()[2], 'm1', 'Standard', '1030125935131234519387311100')
 records = al.getAlignedSeq('ND4') 
 for rec in records:
     n.add(rec.description.split()[2], 'ND4', 'DNA', str(rec.seq))
+# records = al.getAlignedSeq('12S') 
+# for rec in records:
+    # n.add(rec.description.split()[2], '12S', 'DNA', str(rec.seq))
+# records = al.getAlignedSeq('cytb') 
+# for rec in records:
+    # n.add(rec.description.split()[2], 'cytb', 'DNA', str(rec.seq))
 n.writeFile("out")
 
--- src/nexus.py
+++ src/nexus.py
@@ -5,12 +5,13 @@
 ##################################################################################
 
 from string import Template
+import collections
 
 data_template = Template("""#NEXUS
 
 Begin data;
         Dimensions ntax=$ntax nchar=$nchar;
-        Format datacharsete=$datatype interleave=yes gap=- missing=?;
+        Format datatype=$datatype interleave=yes gap=- missing=?;
         Matrix
 $matrix
     ;
@@ -28,44 +29,44 @@ end;
 
 
 ## Problemas
-## - So permite receber agrupado por tipo, nao pode recever DNA, Standard e DNA (sera que e problema? Daria para usar uma lista e checar a ultima entrada
 ## - Pode dar override (Talvez nao seja problema)
 
 # TODO:
-## - Fazer um format com classe
-## - partition dicionario datatype para partititons
+## - Inserir Mensagens de Erro
 
 class NexusWriter:
 
     def __init__(self):
+        self.dna = collections.OrderedDict() 
+        self.standard = collections.OrderedDict() 
+        self.binary = collections.OrderedDict() 
         self.taxa = set()
-        self.data = dict()
-        self._nchar = 1
-        self._format = dict() # { <datatype> : [<begin>, <end>] }
-        self._partition = dict() # { <charset> : [<begin>, <end>, <datatype>] }
+        self.tchar = 0
+        # <charset> : <seq> #
 
     # Function to add a new data entry, recieves a taxon, the type of data and the sequence. 
     def add (self, taxa, charset, datatype, seq):
         # Updating datastructures
+        if datatype == 'DNA':
+            dic = self.dna
+        elif datatype == 'Standard':
+            dic = self.standard
+        elif datatype == 'Binary':
+            dic = self.binary
+        else:
+            print("Error")
+
+        if charset not in dic:
+            self.tchar += len(seq)
+            dic[charset] = dict()
         self.taxa.add(taxa)
-        if charset not in self.data:
-            self._partition[charset] = [str(self._nchar), "", datatype]
-            self.data[charset] = dict()
-            if datatype not in self._format:
-                self._format[datatype] = [str(self._nchar), ""]
-
-            self._nchar += len(seq)
-            self._format[datatype][1] = str(self._nchar - 1)
-            self._partition[charset][1] = str(self._nchar - 1)
-
-        self.data[charset][taxa] = seq
-
+        dic[charset][taxa] = seq
 
     # Function that writes the nexus file
     def writeFile (self, outfile):
         data = data_template.substitute(
             ntax = len(self.taxa), 
-            nchar = self._nchar, 
+            nchar = self.tchar, 
             datatype = self._makeFormat(),
             matrix = self._makeMatrix()
                 )
@@ -82,46 +83,65 @@ class NexusWriter:
     # Function to convert the data matrix into string
     def _makeMatrix (self):
         m = []
-        for t in self.data:
-            for taxa in self.data[t]:
-                m.append(taxa + ' ' + self.data[t][taxa])
-            m.append(" ")
+        for dic in [self.dna, self.standard, self.binary]:
+            for c in dic: 
+                for k in sorted(dic[c]):
+                    m.append(k + ' ' + dic[c][k])
+                m.append(" ")
         return '\n'.join(m)
 
     # Function to convert the format dictionary into string
     def _makeFormat (self):
-        if len(self._format) == 1:
-            return list(self._format)[0];
+        f = []
+        beg = 1
+        end = 0
+        dics = [self.dna, self.standard, self.binary]
+        names = ['DNA', 'Standard', 'Binary']
+        for i in range(len(dics)):
+            if len(dics[i]) >= 1:
+                for ch in dics[i]:
+                    end += self._getSeqLen(ch, dics[i])
+                f.append([names[i], beg, end])
+                beg = end + 1
         out = []
-        for it in self._format.items():
-            out.append(it[0] + ":" + '-'.join(it[1]))
+        if len(f) == 1:
+            return f[0][0]
+        for it in f:
+            out.append(it[0] + ":" + str(it[1]) +'-'+ str(it[2]))
         return "mixed(" + ','.join(out) + ')'
         
     # Function to convert the partition dictionary into string
     def _makePartition (self):
         s = []
-        for cset in self._partition.items():
-            s.append("\tcharset {0} = {1};".format(cset[0], '-'.join(cset[1])))
-        s.append("\tpartition part = {0}: {1};".format(str(len(self._partition)), " ".join(list(self._partition.keys()))))
+        clist = [] 
+        beg = 1
+        end = 0
+        tot = 0
+        for dic in [self.dna, self.standard, self.binary]:
+            for cset in dic:
+                tot += 1
+                end += self._getSeqLen(cset, dic) 
+                s.append("\tcharset {0} = {1}-{2};".format(cset, beg, end))
+                clist.append(cset)
+                beg = end + 1
+
+        s.append("\tpartition part = {0}: {1};".format(tot, ", ".join(clist)))
         s.append("\tset partition = part;")
         return '\n'.join(s)
     
     # Function to convert the partition dictionary into string
     def _makeCode (self):
         c = []
-        part = dict()
-        for d in self._format:
-            part[d] = []
-        i = 1
-        for cset in self._partition.items():
-            # Garante ordem certa??
-            part[cset[1][2]].append(i)
-            i+= 1
-        if 'DNA' in part:
-            c.append("\tlset applyto=({0}) nst=mixed rates=invgamma;".format(",".join(str(x) for x in part['DNA'])))
+        if len(self.dna) >= 1:
+            c.append("\tlset applyto=({0}) nst=mixed rates=invgamma;".format(",".join(str(x) for x in list(range(1, len(self.dna) +1)))))
         c.append("\tunlink revmat=(all) pinvar=(all) shape=(all) statefreq=(all);")
         c.append("\tprset ratepr=variable;")
         return '\n'.join(c)
+
+
+    def _getSeqLen(self, charset, dic):
+        return len(list(dic[charset].values())[0])
+
 
 
 
