d81a802af2c78af33fdba52be360447b19fd0fc2
Victor Wichmann Raposo <vwraposo@gmail.com>
2018-05-11 15:30:59 -0300

    Criando novas tabelas

--- src/snakes.sql
+++ src/snakes.sql
@@ -26,3 +26,56 @@ INSERT INTO snakes(sn_sp, sn_ge) VALUES ('erythromelas', 'Bothrops');
 INSERT INTO snakes(sn_sp, sn_ge) VALUES ('insularis', 'Bothrops');
 INSERT INTO snakes(sn_sp, sn_ge) VALUES ('jararaca', 'Bothrops');
 INSERT INTO snakes(sn_sp, sn_ge) VALUES ('cotiara', 'Bothrops');
+
+
+-- Creating Protein and Peptide:
+
+CREATE TABLE peptides (
+  pep_seq varchar(100) not NULL, 
+  CONSTRAINT pk_pep PRIMARY KEY (pep_seq)
+);
+
+CREATE TABLE proteins (
+  pr_acc varchar(100), 
+  pr_toxclass varchar(100), 
+  CONSTRAINT pk_pr PRIMARY KEY (pr_acc)
+);
+
+CREATE TABLE pep_pr (
+  pep_seq varchar(100), 
+  pr_acc varchar(100),
+  CONSTRAINT fk_pr FOREIGN KEY (pr_acc)
+    REFERENCES proteins(pr_acc)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT fk_pep FOREIGN KEY (pep_seq)
+    REFERENCES peptides(pep_seq)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABlE pr_sn (
+  sn_sp varchar(100), 
+  pr_acc varchar(100),
+  CONSTRAINT fk_pr FOREIGN KEY (pr_acc)
+    REFERENCES proteins(pr_acc)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT fk_sn FOREIGN KEY (sn_sp)
+    REFERENCES snakes(sn_sp)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+CREATE TABLE pep_sn (
+  pep_seq varchar(100), 
+  sn_sp varchar(100),
+  CONSTRAINT fk_sn FOREIGN KEY (sn_sp)
+    REFERENCES snakes(sn_sp)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT fk_pep FOREIGN KEY (pep_seq)
+    REFERENCES peptides(pep_seq)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
