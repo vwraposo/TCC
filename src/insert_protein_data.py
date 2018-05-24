49f52ad85480f09066aed4e051fda5961a402fc9
Victor Wichmann Raposo <vwraposo@gmail.com>
2018-05-24 15:02:06 -0300

    Usando como entrada CSV, permitindo obter informacoes de peptideos e proteinas

--- src/insert_protein_data.py
+++ src/insert_protein_data.py
@@ -7,6 +7,7 @@ import psycopg2
 import sys
 import os
 import xlrd
+import csv
 
 
 try:
@@ -16,46 +17,57 @@ except:
     sys.exit(1)
 
 cur = conn.cursor()
-f = '../data/protein/proteins.xls'
 
-wb = xlrd.open_workbook(f)
-for sh in wb.sheets()[:-1]:
-    species = sh.name[3:]
-    print("Processing sheet from: " + species)
+## CSV
+dirname = "../data/protein"
+directory = os.fsencode(dirname)
+for file in os.listdir(directory):
+    filename = dirname + "/" + os.fsdecode(file)
+    if not filename.endswith(".csv"): 
+        continue;
+    print("Processing: " + filename)
 
-    acc = list(filter(lambda x: x.ctype != xlrd.XL_CELL_EMPTY, sh.col(2)))
-    toxclass = list(filter(lambda x: x.ctype != xlrd.XL_CELL_EMPTY, sh.col(6)))
-    peptides = list(filter(lambda x: x.ctype != xlrd.XL_CELL_EMPTY, sh.col(7)))
+    with open(filename, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        
+        species = str(file).split()[1][:-5]
+        protein = ''
+        toxclass = ''
+        for row in reader:
+            if row['Accession number'] != '' and row['Toxin Class'] != '':
+                protein = row['Accession number']
+                toxclass = row['Toxin Class']
 
-    for i in range(1, len(acc)):
-        try:
-            cur.execute("INSERT INTO proteins(pr_acc, pr_toxclass) VALUES ('{0}', '{1}')\
-                    ON CONFLICT DO NOTHING;".format(str(acc[i].value), str(toxclass[i].value)))
-            cur.execute("INSERT INTO pr_sn(pr_acc, sn_sp) VALUES ('{0}', '{1}')\
-                    ON CONFLICT DO NOTHING;".format(str(acc[i].value), species))
-        except psycopg2.ProgrammingError as e:
-            print("Insert error")
-            print(e)
-            conn.rollback()
-            print("Rollback complete")
+                # New Protein 
+                try:
+                    cur.execute("INSERT INTO proteins(pr_acc, pr_toxclass) VALUES ('{0}', '{1}')\
+                            ON CONFLICT DO NOTHING;".format(protein, toxclass))
+                    cur.execute("INSERT INTO pr_sn(pr_acc, sn_sp) VALUES ('{0}', '{1}')\
+                            ON CONFLICT DO NOTHING;".format(protein, species))
+                except psycopg2.ProgrammingError as e:
+                    print("Insert error")
+                    print(e)
+                    conn.rollback()
+                    print("Rollback complete")
 
-        conn.commit()
+                conn.commit()
 
-    for i in range(1, len(peptides)):
-        try:
-            cur.execute("INSERT INTO peptides(pep_seq) VALUES ('{0}')\
-                    ON CONFLICT DO NOTHING;".format(str(peptides[i].value)))
-            cur.execute("INSERT INTO pep_sn(pep_seq, sn_sp) VALUES ('{0}', '{1}')\
-                    ON CONFLICT DO NOTHING;".format(str(peptides[i].value), species))
-        except psycopg2.ProgrammingError as e:
-            print("Insert error")
-            print(e)
-            conn.rollback()
-            print("Rollback complete")
+             # Add Peptide
+            peptide = row['Peptides']
+            try:
+                cur.execute("INSERT INTO peptides(pep_seq) VALUES ('{0}')\
+                        ON CONFLICT DO NOTHING;".format(peptide))
+                cur.execute("INSERT INTO pep_sn(pep_seq, sn_sp) VALUES ('{0}', '{1}')\
+                        ON CONFLICT DO NOTHING;".format(peptide, species))
+                cur.execute("INSERT INTO pep_pr(pep_seq, pr_acc) VALUES ('{0}', '{1}') \
+                        ON CONFLICT DO NOTHING;".format(peptide, protein))
+            except psycopg2.ProgrammingError as e:
+                print("Insert error")
+                print(e)
+                conn.rollback()
+                print("Rollback complete")
 
-        conn.commit()
-
-
+            conn.commit()
 
 cur.close()
 conn.close()
