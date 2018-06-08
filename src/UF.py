2cb225e34570ccc09b122e57178db6d632e3d85d
Victor Wichmann Raposo <vwraposo@gmail.com>
2018-06-08 13:37:33 -0300

    Modulo com a implemetacao da estrutura de dados Union Find

---
+++ src/UF.py
@@ -0,0 +1,23 @@
+class UnionFind:
+
+    def __init__ (self, n):
+        self.id = list(range(n))
+        self.sz = [1] * n
+
+    def find(self, p):
+        f = self.id[p] 
+        if (f == p):
+            return p;
+        self.id[p] = self.find(f)
+        return self.id[p]
+
+    def union(self, p, q):
+        p = self.find(p)
+        q = self.find(q)
+        if (p == q):
+            return
+        if (self.sz[p] > self.sz[q]):
+            p, q = q, p 
+        self.id[p] = q
+        self.sz[q] += self.sz[p]
+
