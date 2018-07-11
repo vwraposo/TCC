a2377f374121e841afad5156baa1d21628f40a93
Victor Wichmann Raposo <vwraposo@gmail.com>
2018-07-11 10:59:55 -0300

    Refatorando codigo e comentando

--- src/lapointe.py
+++ src/lapointe.py
@@ -7,73 +7,164 @@ import numpy as np
 # O algoritmo de decomposicao parece estranho
 
 # NISI da entre 0 e 1?
+def _checkDMatrix(A):
+    if not np.allclose(A, A.T):
+        raise Exception
+    n, m = np.shape(A)
+    if (n != m):
+        raise Exception
+
+def _checkDMatrixes(A, B):
+    _checkDMatrix(A) 
+    _checkDMatrix(B) 
+
+    nA, m = np.shape(A)
+    nB, m = np.shape(B)
+    if (nA != nB):
+        raise Exception
+
+def standardize(A):
+    _checkMatrix(A)
+    """ Compute matrix with values in [0, 1]
+
+    Parameters
+    ----------
+    A: np.array, 
+        distance matrix
+
+    Returns
+    ----------
+    B: np.array, 
+        standardized matrix
+    """
+    return A / np.max(A)
+
+def getDecomposition(A):
+   """ Compute the decomposition of a distance matrix into the matrix of
+   the ultrametric tree and the matrix of the star tree
+
+    Parameters
+    ----------
+    A: np.array, 
+        distance matrix
+    
+    Returns
+    ----------
+    U: np.array, 
+        distance matrix of the ultrametric tree
+
+    C: np.array, 
+        distance matrix of the star tree
+    """
+    _checkDMatrix(A)
+    n, m = np.shape(A)
+    # Finding center
+    c = 0
+    mn = np.sum(A[c])
+    for i in range(1, n):
+        s = np.sum(A[i])
+        if s < mn: 
+            c = i 
+            mn = s
+
+    radius = np.min(A[A != np.min(A)])
+    U = A.copy()
+    for i in range(n):
+        for j in range(n):
+            if i == j: 
+                continue;
+            U[i][j] = U[i][j] - (A[c][i] - radius) - (A[c][j] - radius)
+
+
+    C = A - U
+    # Check negatives, sum constant
+    
+    return U, C
+
+def NISI(A, B):
+    """ Description
+
+    Parameters
+    ----------
+
+    Returns
+    ----------
+    """
+    _checkDMatrixes(A, B) 
+    n, m = np.shape(A)
+    result = 0
+    for i in range(n):
+        for j in range(n):
+            Cu = 1 - max(A[i][j], B[i][j])
+            Co = abs(A[i][j] - B[i][j]) 
+            MX = max(Cu, Co)
+            result += (Cu - Co) /  MX
+    return (1 + result) / 2
+
+def SSD(A, B):
+    """ Description
+
+    Parameters
+    ----------
+
+    Returns
+    ----------
+    """
+    _checkDMatrixes(A, B) 
+    return np.sum((A-B)**2)
+
+def getDistanceMatrix(t, f):
+    """ Description
+
+    Parameters
+    ----------
+
+    Returns
+    ----------
+    """
+
+    tns = dendropy.TaxonNamespace()
+    tree = dendropy.Tree.get_from_path(t, f, taxon_namespace=tns)
+    pdm = dendropy.calculate.treemeasure.PatrisiticDistanceMatrix(tree)
+    A = []
+    for tax1 in tree.taxon_namespace:
+        A.append(list(map(lambda tax2: pdm(tax1, tax2), tree.taxon_namespace))) 
+    
+    # Make matrix values between 0, 1
+    A = np.array(A)
+    return A
+
+
+
 
 class LL:
+    """ Implementation of the metric of comparison between phylogenetic trees
+    in the Legendre and Lapointe 1992 paper.
+
+    Parameters
+    ----------
+
+    t1: string, 
+        name of a file with a phylogenetic tree
+
+    t2: string, 
+        name of a file with a phylogenetic tree
+
+    f: string, default: nexus, 
+        type of file containing the pyhologenetic tree
+
+    """
 
     def __init__(self, t1, t2, f="nexus"):
-        tns = dendropy.TaxonNamespace()
+        self.dm1 = getDistanceMatrix(t1, f)
+        self.dm2 = getDistanceMatrix(t2, f)
 
-        self.tree1 = dendropy.Tree.get_from_path(t1, f, taxon_namespace=tns)
-        self.n = len(self.tree1.taxon_namespace)
-        self.dm1 = self._getDistanceMatrix(self.tree1, f)
-        a = self._getDecomposition(self.dm1)
-
-        self.tree2 = dendropy.Tree.get_from_path(t2, f, taxon_namespace=tns)
-        self.dm2 = self._getDistanceMatrix(self.tree2, f)
-        a = self._getDecomposition(self.dm2)
+        _checkDMatrixes(self.dm1, self.dm2)
+        a = getDecomposition(self.dm1)
+        b = getDecomposition(self.dm2)
 
 
-    def _getDistanceMatrix(self, tree, f):
-        pdm = dendropy.calculate.treemeasure.PatrisiticDistanceMatrix(tree)
-        dm = []
-        for tax1 in tree.taxon_namespace:
-            dm.append(list(map(lambda tax2: pdm(tax1, tax2), tree.taxon_namespace))) 
-        
-        # Make matrix values between 0, 1
-        dm = np.array(dm)
-        return dm
-
-    def _getDecomposition(self, dm):
-
-        # Finding center
-        c = 0
-        mn = np.sum(dm[c])
-        for i in range(1, self.n):
-            s = np.sum(dm[i])
-            if s < mn: 
-                c = i 
-                mn = s
-
-        radius = np.min(dm[dm != np.min(dm)])
-        U = dm.copy()
-        for i in range(self.n):
-            for j in range(self.n):
-                if i == j: 
-                    continue;
-                U[i][j] = U[i][j] - (dm[c][i] - radius) - (dm[c][j] - radius)
-
-
-        C = dm - U
-        # Check negatives, sum constant
-        
-        return U, C
-
-    def _standardize(self, m):
-        return m / np.max(m)
-
-    def NISI(self):
-        result = 0
-        for i in range(self.n):
-            for j in range(self.n):
-                Cu = 1 - max(self.dm1[i][j], self.dm2[i][j])
-                Co = abs(self.dm1[i][j] - self.dm2[i][j]) 
-                MX = max(Cu, Co)
-                result += (Cu - Co) /  MX
-        return (1 + result) / 2
-
-    def SSD(self):
-        return np.sum((self.dm1-self.dm2)**2)
-
+    
 # Gera Arvores aleatorias 
 
 # Obtem a probabilidade 
