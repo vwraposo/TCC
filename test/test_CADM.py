7d4f12fbbf4c7ee00a5789eacb4dd1df44689235
Victor Wichmann Raposo <vwraposo@gmail.com>
2018-07-30 11:18:04 -0300

    Testando modos do CADM

--- test/test_CADM.py
+++ test/test_CADM.py
@@ -7,3 +7,8 @@ import pytest
 def test_diffNtax() :
     with pytest.raises(Exception):
         CADM("../test/t1.nex", "../test/t6.nex")
+
+def test_modes():
+    with pytest.raises(Exception):
+        CADM("../test/t1.nex", "../test/t2.nex", "-g")
+
