ea8a46a8965336621096d5f689b295ae792f7ca1
Victor Wichmann Raposo <vwraposo@gmail.com>
2018-10-09 17:25:17 -0300

    gerando relatorios entre todos os nos

--- src/id_suppep.py
+++ src/id_suppep.py
@@ -6,7 +6,6 @@
 import psycopg2
 import sys
 import os
-
 import re
 
 def similar (x, y):
@@ -23,42 +22,51 @@ def diff (p, q):
     l = [ch for ch in p if not similar(p[ch], q[ch])]
     return l
 
-directory = "../reports/supp_peptides/"
-with open(directory + "char_matrix.txt") as f:
-    ids = f.readline().split("\t#")
-    ids[0] = ids[0][1:]
-    ids[-1] = ids[-1][:-1]
+def create_report(file1, file2):
+    directory = "../reports/supp_peptides/"
+    with open(directory + "char_matrix.txt") as f:
+        ids = f.readline().split("\t#")
+        ids[0] = ids[0][1:]
+        ids[-1] = ids[-1][:-1]
 
-try:
-    conn = psycopg2.connect(dbname="testdb",  user="fox", password="senha")
-except:
-    print("Error: It was not possible to connect to the database")
-    sys.exit(1)
-
-cur = conn.cursor()
-
-p = parse("../reports/supp_peptides/10.txt")
-q = parse("../reports/supp_peptides/9.txt")
-
-l = diff (p, q)
-
-output = open("../reports/supp_peptides/report.txt", "w")
-output.write("Report comparing the nodes 9 and 10 from the tree. The list below is comprised of the peptides that causes the branching between this nodes\n\n")
-for i in l:
     try:
-        cur.execute("SELECT * FROM peptides WHERE pep_id = {0};".format(ids[int(i)-1]))
-    except psycopg2.ProgrammingError as e:
-        print("Error")
-        print(e)
-        conn.rollback()
-        print("Rollback complete")
-    conn.commit()
+        conn = psycopg2.connect(dbname="testdb",  user="fox", password="senha")
+    except:
+        print("Error: It was not possible to connect to the database")
+        sys.exit(1)
 
-    peptide = cur.fetchone()
-    output.write("Peptide ID: {0} Sequence: {1}\n".format(peptide[0], peptide[1]))
+    cur = conn.cursor()
 
-output.close
 
-cur.close()
-conn.close()
+    p = parse(directory + file1)
+    q = parse(directory + file2)
+
+    l = diff (p, q)
+
+    output = open("../reports/supp_peptides/report_{0}v{1}.txt".format(file1[:-4], file2[:-4]), "w")
+    output.write("Report comparing the nodes {0} and {1} from the tree. The list below is comprised of the peptides that causes the branching between this nodes\n\n".format(file1[:-4], file2[:-4]))
+    for i in l:
+        try:
+            cur.execute("SELECT * FROM peptides WHERE pep_id = {0};".format(ids[int(i)-1]))
+        except psycopg2.ProgrammingError as e:
+            print("Error")
+            print(e)
+            conn.rollback()
+            print("Rollback complete")
+        conn.commit()
+
+        peptide = cur.fetchone()
+        output.write("Peptide ID: {0} \t Sequence: {1}\n".format(peptide[0], peptide[1]))
+
+    output.close
+
+    cur.close()
+    conn.close()
+
+create_report("9.txt", "neuwiedi.txt")
+create_report("10.txt", "9.txt")
+create_report("9.txt", "5.txt")
+create_report("6.txt", "5.txt")
+create_report("2.txt", "5.txt")
+
 
