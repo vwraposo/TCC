05f01ed952fdebbf02c2cb44675398a3d2956de5
Victor Wichmann Raposo <vwraposo@gmail.com>
2018-11-09 09:26:20 -0200

    Melhorando script

--- src/report.py
+++ src/report.py
@@ -10,19 +10,21 @@ import os
 dirname = '../input/'
 directory = os.fsencode(dirname)
 
-mith = dirname + 'genomic/genomic.nex.con.tre'
+gen = dirname + 'genomic/genomic.nex.con.tre'
+
+dont = ['genomic', 'extras', 'de_novo', 'de_novo_HC']
 
 # Compare with others 
 
-print("Report comparing topology using the CADM test of the resulting trees with the tree obtained with the mithocondrial data.")
+print("Report comparing topology using the CADM test of the resulting trees with the tree obtained with the genomic data.")
 print(30*("-"))
 for d in os.listdir(directory):
     dname = os.fsdecode(d)
-    if (not os.path.isdir(dirname + dname)) or (dname == 'genomic') or (dname == 'extras'):
+    if (not os.path.isdir(dirname + dname)) or (dname in dont):
         continue
     print('Target: ' + dname)
     tree = '{0}{1}/{1}.nex.con.tre'.format(dirname, dname)
-    CADM(mith, tree, '-t')
+    CADM(gen, tree, '-t')
 
     print(30*("-"))
     
